d2cf574cf289f157185c2d43e46997fdc97852b7
Mohd Anis Balkhi <135516219+Anishbalkhi@users.noreply.github.com>
2024-07-03 11:49:01 +0530

    array_methods.js

---
+++ array_methods.js
@@ -0,0 +1,61 @@
+//array methods
+// let student = ["anis", "sunny","bunny","ram", "sita"]
+
+
+//push method
+// student.push("lukcy")
+// console.log(student)
+
+
+//pop method
+//it delete the top most element
+//student.pop()
+//console.log(student)
+
+
+//unshift method
+//it adds the element at the starting of array
+//student.unshift("bala")
+//console.log(student)
+
+
+//shift method
+//student.shift()
+//console.log(student)
+
+// let colour = ["yellow", "red", " orange", "green"]
+
+//indexOf method: it returns the index of values
+//includes method : it returns that element is exist or not
+
+// console.log(colour.indexOf("green"))
+// console.log(colour.includes("red"))
+// console.log(colour.includes("black"))
+
+//concat method: use for adding two arrays
+//  let numbers = [1,2,3,4,5,6]
+// let add = numbers.concat(colour)
+// let adds = colour.concat(numbers)
+// console.log(add)
+// console.log(adds)
+
+//revrse method
+
+// console.log(numbers.reverse())
+
+//slice method: it select a portion of array and give us a copy of orignal array
+//  let num = [1,2,4,5,6,7,8]
+// console.log(num.slice(1,5))
+// console.log(num.slice(3))
+
+//splice method: use for remove/replace/add elements
+// num.splice(0,1)
+// console.log(num)
+// num.splice(0,1,"w","z")
+// console.log(num)
+
+//sort method: this method is only use for character or strings
+// let cars = ["maruti", "xuv", "mahendra"]
+// cars.sort();
+// console.log(cars)
+
