bc0d9e2a082eb1261cb64017ac6fddb08d3be4a1
Mohd Anis Balkhi <135516219+Anishbalkhi@users.noreply.github.com>
2024-07-04 11:36:26 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,80 @@
+//loops
+//for loop
+// for(let i = 1; i<=10; i++){
+//     console.log(i);
+// }
+
+//print odd numbers(1 to 15)
+
+// for(let j = 1; j<=15; j+=2){
+//     console.log(j)
+// }
+
+//print reverse of odd numbers(15 to 1)
+// for( let j = 15; j>=1; j-=2){
+//     console.log(j)
+// }
+
+//print even numbers 
+// for( let j = 2 ; j<=20; j+=2){
+//     console.log(j)
+// }
+
+//infinte loop : never run any infinte loop in your system
+//ex:
+//i use : in the place of ; for saftey
+//for(let i = 1 : i<=0 : i++)
+//for(let i= 1 :  : i++ )
+
+//print the table of any number
+// //please try on console for better results
+// let n = prompt("enter your value");
+//we use prompt for taking inputs form user
+// n = parseInt(n);
+// for(let i =n; i<=n*10; i= i+n ){
+//     console.log(i);
+// }
+
+//nested loop
+// for( let i = 0; i<=3; i++){
+//     console.log(`outer loop `)
+//     for(let j = 0; j<=5; j++){
+//         console.log(j)
+//     }
+// }
+
+//while loop
+// let i = 1
+// while(i<=10){
+//     console.log(i);
+//     i++
+// }
+
+//ex.
+//favorite movie
+//run on console
+//not completly correct
+// let favorite = "tumbaad"
+// let n = prompt(`enter the favorite movie`);
+// while(n!=favorite && n!= "quit")
+// {
+//     console.log("wrong");
+//     n = prompt(`please enter the right guess`);
+// }
+// if(n = favorite){
+// console.log("good");}
+// else{
+//     console.log("you quit");
+// }
+
+
+//break keyword
+// for(let i = 1; i<=10; i++){
+//     if(i ==5){
+//         break;
+//     }
+//     console.log(i);
+// } 
+
+
+//
