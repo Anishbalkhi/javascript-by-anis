a90867d40c2b8f5075ba3b00918e81a9078b3ffd
Mohd Anis Balkhi <135516219+Anishbalkhi@users.noreply.github.com>
2024-07-06 10:41:17 +0530

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,99 @@
+// // // let student = {
+// // //     name: "anish",
+// // //     age: 20,
+// // //     roll_no: 103
+// // // }
+// // // console.log(student.name)
+
+// // // const item = {
+// // //     price: 100,
+// // //     discount: 20,
+// // //     colors: ["green","red"]
+// // // }
+// // // //access the data of object.
+// // // //there are two syntax to write it 
+// // // //1. obj.key
+// // // //2. obj["key"]
+// // // console.log(item.price)
+// // // console.log(item["discount"])
+// // // //print the whole object
+// // // console.log(item)
+
+// // // //[] is convert any value in string where dot(.) is not do that
+
+// // // //how change the values of key and create a new key
+// // // let women = {
+// // //     name: "divya",
+// // //     age: 32,
+// // //     husband: "vivek bindra"
+// // // }
+// // // //changing the value of key
+// // // console.log(women.husband)
+// // // women.husband = "sandeep mahesvari"
+// // // console.log(women.husband)
+
+// // // //create the key
+// // // women.address = "jaipur"
+// // // console.log(women)
+// // // //delete any key
+// // // delete women.husband
+// // // console.log(women)
+
+ 
+// // //nested object
+// // let college = {
+// //     student: {
+// //         name: "anish",
+// //         age: 20,
+// //         address: "nagour"
+// //     },
+// //     teacher: { 
+// //         name: "mahesh",
+// //         age: 30,
+// //         subject: "oops"
+// //     }
+// // }
+
+// // console.log(college)
+// // console.log(college.student.name)
+// // console.log(college.teacher.subject)
+
+
+// //array of objects
+// let Classinfo = [
+//     {//they are empty object
+//         name: "aman",
+//         age: 25,
+//         add: "delhi"
+//     },
+//     {
+//         name: "manish",
+//         age: 26,
+//         add: "up"
+//     }
+// ]
+
+// console.log(Classinfo[0])
+// console.log(Classinfo[1].name)
+
+// //math objects or methods
+// //use for abslute  value
+// let a = Math.abs(-12)
+// console.log(a)
+// //for power
+// let b = Math.pow(2,4)
+// console.log(b)
+
+// //for roundoff :it round off by decrissing the number
+// let c = Math.floor(12.34)
+// console.log(c)
+
+// //for roundoff : it round off by incrissing the number
+// let d = Math.ceil(12.34)
+// console.log(d)
+
+// //for given any random value : it not genrate any integer value
+// let e = Math.random()
+// console.log(e)
+
+
